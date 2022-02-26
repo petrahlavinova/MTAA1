9f6c4bf9cdd622e7e12ac086fdfcc906617929e1
petrahlavinova <71736002+petrahlavinova@users.noreply.github.com>
2022-02-26 20:05:27 +0100

    init

---
+++ zadanie1.py
@@ -0,0 +1,9 @@
+import socketserver
+import lib
+HOST = '0.0.0.0'
+PORT = 5060
+
+if __name__ == "__main__":  
+    lib.prepare()  
+    server = socketserver.UDPServer((HOST, PORT), lib.UDPHandler)
+    server.serve_forever()
